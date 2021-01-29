220ed51e29878d11a82b38a1c66e57da7e161a81
sen <ochieeochieeochiee@gmail.com>
2021-01-29 19:49:05 +0900

    ifまで

--- Count2/ViewController.swift
+++ Count2/ViewController.swift
@@ -9,11 +9,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var label: UILabel!
+    var number: Int! = 0
+
+    func showLabel() {
+        label.text = String(number)
+        label.font = UIFont(name: "Charter-Roman", size: 40)
+        if number >= 10 {
+            label.textColor = UIColor.red
+        }else if number <= -10 {
+            label.textColor = UIColor.blue
+        }else{
+            label.textColor = UIColor.gray
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        number = 0
+        showLabel()
     }
 
+    @IBAction func plus() {
+        number += 1
+        showLabel()
+    }
+    @IBAction func minus() {
+        number = number - 1
+        showLabel()
+    }
+    
+    @IBAction func double() {
+        number = number * 2
+        showLabel()
+    }
+    
+    @IBAction func devide() {
+        number = number / 2
+        showLabel()
+    }
+    
+    
 
 }
 
